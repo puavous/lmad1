d4669ba6e1651715f3a2e0b04eb370ca9990ba2b
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2019-03-02 19:34:29 +0200

    After-touching at Instanssi

---
+++ define_Invasion.mk
@@ -0,0 +1,13 @@
+## Short and long name of this production and the name of its author:
+# Filename stub for source and target files
+PROD_NAME=invasion
+# A "full" name of the production, used in zip package name
+PROD_NAME_FULL=Flat_Ball_Invasion_Fleet
+# Handle of the author, also included in the zip package name
+PROD_AUTHOR=The_Old_Dude
+# Path to source files of this production.
+# Must-have files: $(PROD_SRC_PATH)/($PROD_NAME){.js,_song.js,.nfo}
+PROD_SRC_PATH=./prods
+
+# These work for Paavo only, sry.. will fix later:
+CLOS=java -jar /home/nieminen/files/hacking/closure-compiler/closure-compiler-v20170218.jar
