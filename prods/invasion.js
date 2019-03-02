a3d4ec75736110445ddfaa339f4307c6915867fd
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2019-03-02 18:26:50 +0200

    My own production at 30 minutes to absolute deadline.. pressure.

---
+++ prods/invasion.js
@@ -0,0 +1,255 @@
+/* -*- mode: javascript; tab-width: 4; indent-tabs-mode: nil; -*- */
+
+/**
+ * My own entry to Instanssi 2019 compo.. I hope..
+ *
+ **/
+
+/*
+The visuals of this production are synced to the following song
+created using the great SoundBox minisynth:
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7d2xSsNAHMfx3921acVSEQUXhy4OHXwN8REEB1cHBQuOFoNLDAkiFcSt4Ct0dPUZnH2TeE1iKRZ1Uezh90Mud_fPDf87LlluyGRd6qrXVHFufJFRo91XyXalnuaYkk1886sCAAAAAAAAAAAAhMSMY7nitCG1mttVyBbSVt_JlM9nNNBwMPBXVQ8vZ31f4ji-Wvap_nX-9pvnx4HnPw48_2e-BgAAAAAAAAAA_DtulMu-xk5qb7R8d-KMvUhWe_d2172PqY_K8qlMWao0TRMlSd3PUh9RmtzWw2_qOq_rjFUGAAAAAAAAAADAMjqMpaO4I3W0X0Vsob3rM_cURYrU9Ix1DR-vjsJG5X16DFYdgd3NRZfbx_xDM59_teoPs3YI658v7Jc02PUPd_9o4X0NcS4AAAAAAAAAAPycvCrF48qOb60ZY9svm8WBOYl811ivHvjZ_8hC-U9ZEmjeAAAAAAAAAAAA-B1v
+
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dtPSsNAFMfx38y0acWiiIKbLrpx0YXXEI8guPACChZcWgxuYkgQaUG8R8GN93DtTeLkjyW00pWLDn4_yWTmPWYgeSSrIYsDaahRV8Wd8U1Gnf5YFbsnjdRiGu2xTXywqQEAAAAAAAAAAAAhMe-xXHHTkXrdYZ2yhXQ8djLLOb_QRNPJxJ91P31Yxr7FcfxIaQEAAAAAAAAAABASN8tlv2In9Q97Plw4Y--T3dGrPXXteat_nCkvZcpSpWmaKEmaOEt9Rmny0ix8bvq86TNqDgAAAAAAAAAAgG10GUtX8UAa6LzO2EJnT7fuI4oUqeuPH-XYWNepgnpjbFZdy02xekNs3sput9X7D037_uuqvy3HIdQ_X3tf0mDrH-77o7XvNcRnAQAAAAAAAADg7-R1K2Y7J360b4ztfx4VF-Y68qEpT7tp-TzQx06qYz0LAAAAAAAAAACA_-kb
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dzBSsMwHMfxX5LNTR2CePCyw647ePIdxKs3wYMvoODAo8PgpZYWkQ3E9xh48WH0DXyE2jV1lDEGDtwsfD_tv8k_JJCE3kI72Ze66jWV3Zo8ZNRo91Wwe1JPFaZUrdsoT5YFAAAAAAAAAAAAUCfmzctl1w2p1eyGJptJh30nM-uzgAYaDgb5Hcrh_SzPw3v_sKkFAQAAAAAAAAAAAKtwo1T20zupfdDK04kz9i7a7b3YI1ftN__FmdKpREmsOI4jRVGZJ3Heojh6Lgc-lWValsmaFgYAAAAAAAAAAAD8yoWXLn1H6ug0tNhMJ4837n2rqXD9mNaNdY0iCQdjo-I5PRQLB2LjSuv_Nj__uqnOP-z666xeh_0Pe159X-KNzWUVdX9_AAAAAAAAAADAImmIzG-f5bVjY-zOlz7OzdWWir81muXDx2uYIgAAAAAAAAAAAPDXvgE
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dxPSsNAFMfx38z0H1oEUXDTRbdduPIO4tad4MILKFhwaXFwE0OCSAviDTxAwY2X8QYeIU4zsYQqXTfl-0leZt4wA-ERshmS-b400LCt4s6EkFGrN1LJ7klD1ZhKvW-TkKwLAAAAAAAAAAAAoEnMh5crblpStz2IQ7aQjkZOZjnnHxprMh6HM7aTh2Uewnv_SGkBAAAAAAAAAADQJG6ay355J_UOuiGdO2Pvk93hqz129XmrX5wpX8iUpUrTNFGSVHmWhhGlyUu18Llq86rNqDkAAAAAAAAAAAA20aWXrnxf6ussjthCp0-37rPTVjx-LfrGulaZxI2xaXldbIrFDbFZbXSzrd5_09TvP1b9bdlvQv3zP89L2tj657xGAAAAAAAAAADYEnmMojg8D70TY-zOt94vzHVH5d8azfrlMyoIAAAAAAAAAACALfAD
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dsxSsRAFMbxLzOJu-AiiIUWFmm38BriHSw8gWDA0sXBJg4JIiuI91iw8TLeJE4yMcQVrDfy_5HJzHtMYHiENI9sDqUT5ama2yQMJUrnS3XMgZSrl_TGa1OG4K8BAAAAAAAAAAAATEny7mSb61SaZacxZRrpeGn1o1E2pkKroghXnFf3QxyGc-6BsgIAAAAAAAAAAGBK7LqW-XRWmh_NQrixibkr9_NXc2a_92z_aaa6Vany8t6XKss-rnzIyJfP_YNP_Vz3c0W9AQAAAAAAAAAAsIsunXTlFtJCFzFjGp0_3tiPvUzZsK1dJ8amXRCbYuvu3jbEYjPsZZTdbdvnn5rx-WPV34b1FOpf_3pf_GTrX_MJAQAAAAAAAADgX_gC
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dtBSsNAFMbxb2ZSW7AIIuLGRbZdeA3xDi48gWDBpcXBTRwSRCqI9yi48TLeJE4yMYYKrhv5_5LJ5D2GMDxCNo9sDqVj5ZnqWxOHjLLZQi17IOXNjfk2DGwRg78GAAAAAAAAAAAAMCbm3cvV15k0nZymlK2lk4WT-WmZDWip1XIZzzSv7vs4Du_9AzUFAAAAAAAAAADAmLh1JfvpnTQ7msZw44y9K_bzV3vm2gXb_5ipapQqg0IIhYqii8sQMwrFc_fkp26uurmk2AAAAAAAAAAAANhFl1668nNprouUsbXOH2_cx16_ZhIPY13WBqkjtm6vTTcsdcJeBtndtr3_sRnuP1X9rb8fQ_2rX-9LGG39K74fAAAAAAAAAAD8C18
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dtBSsNAFMbxb2ZSW7AIIuLGRbZdeA3xDi48gWDBpcXBTRwSRCqI9yi48TLeJE4yMYYKrhv5_5LJ5D2GMDxCNo9sDqVj5ZnqWxOHjLLZQi17IOXNjfk2DGwRg78GAAAAAAAAAAAAMCbm3cvV15k0nZymlK2lk4WT-WmZDWip1XIZzzSv7vs4Du_9AzUFAAAAAAAAAADAmLh1JfvpnTQ7msZw44y9K_bzV3vm2gXb_5ipapQqg0IIhYqii8sQMwrFc_fkp26uurmk2AAAAAAAAAAAANhFl1668nNprouUsbXOH2_cx16_ZhIPY13WBqkjtm6vTTcsdcJeBtndtr3_sRnuP1X9rb8fQ_2rX-9LGG39K74fAAAAAAAAAAD8C18
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dlBSsNAFMbxb2aiLViE4sKNi2y78BriHVx4AsGCS4uDm3FIEKkg3qPgxst4kzjJhFBacN3I_0deZt4jgeERsnmbuTRXWah5MClkVEwX6thTqWw3ZpcNqfpXAAAAAAAAAAAAAGNivrxcc1dIk6OLXLKNdL5wMnvjMi21Wi7TldfV05Cn8N4_008AAAAAAAAAAACMiVvXsj_eSdOzSUo3ztjHcFJ-2Es3PDRMy-pWpSoqxhgUQp9XMVUUw1v_wmu_1v1a0WgAAAAAAAAAAAAcohsv3fqZNNN1rthGVy_37vu4y4x1RRtdkqdh6-7eTsLyFOx9q3rYds8_Ntvnz13_HPZj6H-9973EUfW_5n8BAAAAAAAAAMC_8ws
+
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dlBSsNAFMbxLzOJLViE4sKNi2xdiLcQ7-DCEwgGXFocuhmHBJEK4j269DKCB4mTTiglBdeN_H_k5c28QBgeIZu3nktzlbnaxyyGMuXTC22YE6nsFtmAMT5W_woAAAAAAAAAAABgTLIfJ9veF9KkOE8l00pnV1ZmOC2TKi2qKl4pL563-xjOuSX9BAAAAAAAAAAAwJjYVSPz7aw0PZ3E7dpm5skflx_m0u5Py5pOrToohODlfb-vQ6wo-Lf-ra99bvpc02gAAAAAAAAAAAAcolsn3bmZNNNNqphW1y8P9uuoUKHM2LyL9ChNw1abezcJS1Ow953qYRuef2x2z5-6_rldj6H_zd73EkbV_4b_BQAAAAAAAAAA_84v
+
+
+http://sb.bitsnbites.eu/?data=U0JveAwC7dkxSgQxFMbxL8muu7CLzbYW01qItxDvYOEJBAcsXQw2MWQQccGLWHoZwYOMGZOVKcTawf-Peby8RxgmYbqnjbRSM1N_Y3LIaLY81hd7KDXDwoyEXP8WAAAAAAAAAAAAwJSYDy_XX82lxfyotGwvNadOdjwna7Vt2_yUvL37rnN47--5SQAAAAAAAAAAAEyJe-5k372TlptFLl-dsbdh1bzYEzeek3WDpBQVYwwKodYp5o5ieKrve6y5qzlxxQAAAAAAAAAAAPiLLrx06dfSWuelY3udPVy7twNjjd1vK3OwMgUbZmCp5qGzm8Axf_r-vR1_AQAAAAAAAAAAwL_zCQ
+
+*/
+
+
+// --------------------------------------------------------------------------------
+// Variables used in this production, specifically:
+var songBeatsPerMinute = 146;
+
+// --------------------------------------------------------------------------------
+var defaultLightDirection = [0,.2,1.5,1];
+var clearColor = [1,1,1,1];
+
+// You can change/add whatever you want here:
+var objTunnel, objBall;
+
+
+// --------------------------------------------------------------------------------
+
+/**
+ * Initialize the constant and pre-computed "assets" used in this
+ * production; this function is called once before entering the main
+ * loop.
+ *
+ * Things like graphics primitives / building blocks can be generated
+ * here.
+ */
+function initAssets(){
+    testpoints = [
+        0,1,0,1, // node
+       -1,1,0,1,
+       -1,1,0,1,
+       -1,0,0,1, // node
+       -1,-1,0,1,
+       -1,-1,0,1,
+        0,-1,0,1, // node
+        1,-1,0,1,
+        1,-1,0,1,
+        1, 0,0,1, // node
+        1, 1,0,1,
+        1, 1,0,1,
+        0, 1,0,1, // node
+    ];
+
+    objTunnel = new GenCyl(new funBezierCurve(matmul4(scaleXYZ_wi(.1,100,.1),testpoints)),
+                        32,
+                        new funBezierCurve(matmul4(scaleXYZ_wi(16,4,1),testpoints))
+                       );
+
+    // Ball can be built from circle curves:
+    objBall = new GenCyl(new funCircle(1,10,.5), 32,
+                         new funCircle(0,32));
+
+/*
+    // Can make the radius negative to make an interior of a ball:
+    objBackground = new GenCyl(new funCircle(-10,10,.5), 32,
+                               new funCircle(0,32));
+*/
+/*
+    // Create primitive building blocks by different profile sweeps:
+    // Now I have a box in the library.
+    objBox = new Box(1);
+*/
+
+}
+
+function buildParpuuppari(){
+    var r ={f:[],o:[],c:[]};
+    //Wings
+    r.c.push({f:[translate_wi(2,0,0),scaleXYZ_wi(.15,1,2)],
+              o:[objBall],
+              c:[]});
+    r.c.push({f:[translate_wi(-2,0,0),scaleXYZ_wi(.15,1,2)],
+              o:[objBall],
+              c:[]});
+    //Cockpit
+    r.c.push({f:[scaleXYZ_wi(.2,.2,.4)],
+              o:[objBall],
+              c:[]});
+    //Truss
+    r.c.push({f:[scaleXYZ_wi(2,.05,.05)],
+              o:[objBall],
+              c:[]});
+
+    return r;
+}
+
+/** Armada at time t */
+function buildArmaada(t){
+    var r ={f:[],o:[],c:[]};
+    var parpuuppari = buildParpuuppari();
+
+    //First scouts at z=0 and z=-200
+    r.c.push({f:[translate_wi(-1,1,0), rotZ_wi(.2)],
+              o:[],
+              c:[parpuuppari]});
+    r.c.push({f:[translate_wi(-4,1,-70), rotZ_wi(-.08)],
+              o:[],
+              c:[parpuuppari]});
+    r.c.push({f:[translate_wi(3,-.5,-80), rotZ_wi(.12)],
+              o:[],
+              c:[parpuuppari]});
+
+    //Multiple ships are coming in after that, beware...
+    var nships = 100;
+    for (var i = 0; i < nships ; i++){
+        // Disperse the ships to full width and height of tunnel; each has its own roll
+        // Make more ships appear in the end than in front
+        zloc = Math.sqrt((i+1)/nships) * nships*3;
+        r.c.push({f:[translate_wi(1+12*Math.sin(i), .5+1.5*Math.sin(i*5), -100 - zloc),
+                     rotZ_wi(Math.sin(i)*t*.4 + i)],
+                  o:[],
+                  c:[parpuuppari]});
+    }
+
+    return r;
+}
+
+
+/**
+ * Return a scene graph for a specific time. Time given as 'beats' according to song tempo.
+ *
+ * This is an important function to re-write creatively to make your own entry.
+ *
+ */
+function buildSceneAtTime(t){
+
+    // Initialize empty scenegraph. Root node with nothing inside:
+    var sceneroot={f:[],o:[],c:[]};
+
+    var some_color=
+        [.1, .12, .05,  1,  // ambient rgba
+         .1, .40, .20,  1,  // diffuse rgba
+         .2, .80, .20,  1,  // specular rgba
+         20,  1,  .00,  0]; // shininess, "unused", mesh, "unused"
+
+    var other_color=
+        [.1, .12, .05,  1,  // ambient rgba
+         .5, .20, .20,  1,  // diffuse rgba
+         .9, .9, .9,  1,  // specular rgba
+         20,  1,  .00,  0]; // shininess, "unused", mesh, "unused"
+
+
+    var ctausta=
+        [.1, .1, .2, 1,
+         .3, .0, .0, 1,
+         .0, .0, .0, 1,
+         .0, .1, .4, 0];
+
+    //var parpuuppari = buildParpuuppari();
+    var armaada = buildArmaada(t);
+
+    var tunnel_length = 60.0;
+    var beat_at_tunnel_exit = 80;
+    var beat_at_roll_end = 32;
+    var linear_time_to_exit = 1 - (beat_at_tunnel_exit - t)/beat_at_tunnel_exit;
+    var scaled_time_to_exit = linear_time_to_exit * linear_time_to_exit;
+    var travelled_in_tunnel_now = (scaled_time_to_exit * tunnel_length);
+    var t_rot = clamp01((beat_at_roll_end - t)/beat_at_roll_end);
+    t_rot = t_rot*t_rot;
+
+    // Slowly move camera towards end of the tube
+    var camera_pos = translate_wi(0,0,-travelled_in_tunnel_now);
+    // Just some shake-ish minor movement:
+    //    var camerashake = translate_wi(Math.sin(t*.16),Math.sin(t*.18+4),0);
+
+    var camera_roll = rotX_wi(PI*t_rot/6);
+    var camera_transformations = [camera_pos,rotZ_wi(PI*t_rot/6),rotX_wi(PI*t_rot/6)];
+
+    sceneroot.c.push({f:[],
+                      o:[],
+                      c:[
+
+                          {f:[],
+                           o:[new Material(some_color),objTunnel],
+                           c:[]},
+
+                          {f:[translate_wi(0,0,-260+t*9)],
+                           o:[new Material(some_color)],
+                           c:[armaada]},
+
+                          {f:[translate_wi(-.7,.5,-660+t*9)],
+                           o:[new Material(other_color)],
+                           c:[armaada]},
+
+                          {f:[translate_wi(.5,-.7,-683+t*9)],
+                           o:[new Material(other_color)],
+                           c:[armaada]},
+
+
+                          {f:camera_transformations,
+                           o:[],
+                           c:[],
+                           r:[new Camera()]
+                          },
+                      ]
+                     }
+                    );
+
+    return sceneroot;
+}
+
+
+
+
+// --------------------------------------------------------------------------------
+
+/**
+ * (Optionally) initialize additional HTML and CSS parts of the
+ * document. This can be used, for example, for scrolling or flashing
+ * text shown as usual HTML or hypertext. Not often used in actual
+ * demoscene productions.
+ */
+function initDocument(){
+}
+
+/**
+ * (Optionally) update the HTML and CSS parts of the document. This
+ * can be used for scrolling or flashing text shown as usual HTML. Not
+ * often used in actual demoscene productions.
+ */
+function updateDocument(t){
+}
---
+++ prods/invasion_song.js
@@ -0,0 +1,226 @@
+    // This music has been exported by SoundBox. You can use it with
+    // http://sb.bitsnbites.eu/player-small.js in your own product.
+
+    // See http://sb.bitsnbites.eu/demo.html for an example of how to
+    // use it in a demo.
+
+    // Song data
+    var song = {
+      songData: [
+        { // Instrument 0
+          i: [
+          0, // OSC1_WAVEFORM
+          255, // OSC1_VOL
+          116, // OSC1_SEMI
+          1, // OSC1_XENV
+          0, // OSC2_WAVEFORM
+          255, // OSC2_VOL
+          116, // OSC2_SEMI
+          0, // OSC2_DETUNE
+          1, // OSC2_XENV
+          0, // NOISE_VOL
+          4, // ENV_ATTACK
+          8, // ENV_SUSTAIN
+          41, // ENV_RELEASE
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          0, // LFO_AMT
+          0, // LFO_FREQ
+          0, // LFO_FX_FREQ
+          2, // FX_FILTER
+          14, // FX_FREQ
+          0, // FX_RESONANCE
+          0, // FX_DIST
+          32, // FX_DRIVE
+          0, // FX_PAN_AMT
+          0, // FX_PAN_FREQ
+          0, // FX_DELAY_AMT
+          0 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [,,,,,,,,1,1,1,1,1,1,2],
+          // Columns
+          c: [
+            {n: [135,,,,135,,,,135,,,,135,,,,135,,,,135,,,,135,,,,135],
+             f: []},
+            {n: [],
+             f: []}
+          ]
+        },
+        { // Instrument 1
+          i: [
+          1, // OSC1_WAVEFORM
+          159, // OSC1_VOL
+          128, // OSC1_SEMI
+          0, // OSC1_XENV
+          3, // OSC2_WAVEFORM
+          255, // OSC2_VOL
+          109, // OSC2_SEMI
+          4, // OSC2_DETUNE
+          0, // OSC2_XENV
+          0, // NOISE_VOL
+          7, // ENV_ATTACK
+          5, // ENV_SUSTAIN
+          28, // ENV_RELEASE
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          0, // LFO_AMT
+          0, // LFO_FREQ
+          0, // LFO_FX_FREQ
+          2, // FX_FILTER
+          255, // FX_FREQ
+          0, // FX_RESONANCE
+          0, // FX_DIST
+          23, // FX_DRIVE
+          41, // FX_PAN_AMT
+          3, // FX_PAN_FREQ
+          0, // FX_DELAY_AMT
+          1 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [,,,,1,1,1,1,1,1,1,1,1],
+          // Columns
+          c: [
+            {n: [113,,125,113,113,125,113,113,113,,125,113,113,125,125,127,113,,125,113,113,125,113,113,113,125,113,113,128,128,128,130],
+             f: []}
+          ]
+        },
+        { // Instrument 2
+          i: [
+          3, // OSC1_WAVEFORM
+          146, // OSC1_VOL
+          140, // OSC1_SEMI
+          0, // OSC1_XENV
+          2, // OSC2_WAVEFORM
+          224, // OSC2_VOL
+          128, // OSC2_SEMI
+          3, // OSC2_DETUNE
+          0, // OSC2_XENV
+          0, // NOISE_VOL
+          8, // ENV_ATTACK
+          19, // ENV_SUSTAIN
+          7, // ENV_RELEASE
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          3, // LFO_WAVEFORM
+          179, // LFO_AMT
+          3, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          2, // FX_FILTER
+          124, // FX_FREQ
+          135, // FX_RESONANCE
+          11, // FX_DIST
+          32, // FX_DRIVE
+          150, // FX_PAN_AMT
+          2, // FX_PAN_FREQ
+          45, // FX_DELAY_AMT
+          3 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [,,,,,,,,1,1,1,1,1],
+          // Columns
+          c: [
+            {n: [140,140,140,140,140,139,,139,137,,137,137,137,135,,135,135,140,140,140,140,139,,139,139,137,137,,137,,137,135,144,,,,,,,,142,,,,,,,,140,,,,,,,,139],
+             f: []}
+          ]
+        },
+        { // Instrument 3
+          i: [
+          0, // OSC1_WAVEFORM
+          91, // OSC1_VOL
+          128, // OSC1_SEMI
+          0, // OSC1_XENV
+          0, // OSC2_WAVEFORM
+          95, // OSC2_VOL
+          128, // OSC2_SEMI
+          12, // OSC2_DETUNE
+          0, // OSC2_XENV
+          0, // NOISE_VOL
+          12, // ENV_ATTACK
+          0, // ENV_SUSTAIN
+          72, // ENV_RELEASE
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          0, // LFO_AMT
+          0, // LFO_FREQ
+          0, // LFO_FX_FREQ
+          2, // FX_FILTER
+          255, // FX_FREQ
+          0, // FX_RESONANCE
+          70, // FX_DIST
+          136, // FX_DRIVE
+          112, // FX_PAN_AMT
+          3, // FX_PAN_FREQ
+          187, // FX_DELAY_AMT
+          6 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [6,,6,,5,5,5,5,1,2,3,4],
+          // Columns
+          c: [
+            {n: [144,,,,,,146,,,,,,140,,,,139,,,,,,147,,,,,,146],
+             f: []},
+            {n: [144,,,,,,146,,,,,,140],
+             f: []},
+            {n: [144,,,,,,146,,,,,,147,,,,,,152,,,,,,147,,,,146],
+             f: []},
+            {n: [140,,,,,,147,,,,,,146,,,,,,137],
+             f: []},
+            {n: [144,,,,,,146,,,,,,140],
+             f: []},
+            {n: [144,,,,,,,,,,,,146,,,,,,,,,,,,140],
+             f: []}
+          ]
+        },
+        { // Instrument 4
+          i: [
+          0, // OSC1_WAVEFORM
+          0, // OSC1_VOL
+          140, // OSC1_SEMI
+          0, // OSC1_XENV
+          0, // OSC2_WAVEFORM
+          0, // OSC2_VOL
+          140, // OSC2_SEMI
+          0, // OSC2_DETUNE
+          0, // OSC2_XENV
+          255, // NOISE_VOL
+          162, // ENV_ATTACK
+          9, // ENV_SUSTAIN
+          37, // ENV_RELEASE
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          15, // LFO_AMT
+          1, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          2, // FX_FILTER
+          8, // FX_FREQ
+          214, // FX_RESONANCE
+          20, // FX_DIST
+          255, // FX_DRIVE
+          88, // FX_PAN_AMT
+          1, // FX_PAN_FREQ
+          108, // FX_DELAY_AMT
+          6 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [,,,1,2,2,2,2,,,,,,3],
+          // Columns
+          c: [
+            {n: [135,,,,,,,,,,,,,,,,,,,,,,,,135],
+             f: []},
+            {n: [135,,,,,,,,,,,,,,,,135],
+             f: []},
+            {n: [135],
+             f: []}
+          ]
+        },
+      ],
+      rowLen: 4531,   // In sample lengths
+      patternLen: 32,  // Rows per pattern
+      endPattern: 14,  // End pattern
+      numChannels: 5  // Number of channels
+    };
