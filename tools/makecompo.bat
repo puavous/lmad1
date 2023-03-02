7fb057d0d6dc369eecc96a03c830326689f132c6
Paavo Nieminen <paavo.j.nieminen@jyu.fi>
2023-03-02 02:56:19 +0200

    Trying out a Windows-only toolset. Today's latest versions of some spotted and scripted.

--- tools/makecompo.bat
+++ tools/makecompo.bat
@@ -1,6 +1,12 @@
 @REM For the Windows command line user: this builds the compo version.
+@REM Local run will need insecure browser session. For Chrome:
+@REM chrome.exe --disable-web-security --disable-gpu --user-data-dir=C:\\MyTemp\\chromeTemp
 
-@REM TODO: Batch files for compo version and entry package. Better configuration.
+@REM NOTE: This isn't very good yet. In Instanssi 2023, we will be
+@REM much better off by just installing Windows Subsystem For Linux
+@REM and start creating!
+
+@REM TODO: Easier configuration..
 
 @REM Path to lmad1 location in your system (can be ".." if you work in a subdirectory of the example package)
 @SET LMAD1=..
@@ -16,9 +22,9 @@
 @REM Order of files matters because of catenation etc:
 @SET LMAD1_COMMONSRC=%LMAD1%\lib\library.js %LMAD1%\lib\shaders_simple.js %LMAD1%\lib\minified_shaders.js %LMAD1%\external\player-small.js
 
-@SET sed=%LMAD1%\winext\sedbin\sed.exe
+@SET sed=%LMAD1%\external\windows-sed.exe
 @SET closurecomp=java -jar %LMAD1%\external\closure-compiler.jar
-@SET jsexe=%LMAD1%\winext\jsexe.exe
+@SET jsexe=%LMAD1%\external\jsexe.exe
 
 @echo (function (){ > tmp.bulk.compo.js
 @type %LMAD1%\lib\glconstants.js %LMAD1_COMMONSRC% song.js prod.js %LMAD1%\lib\main.js >> tmp.bulk.compo.js
@@ -26,7 +32,7 @@
 
 @type tmp.bulk.compo.js | %sed% -f %LMAD1%\tools\prep.sed | %sed% -f %LMAD1%\tools\shortengl.sed | %sed% -f %LMAD1%\tools\shortenplayer.sed | %closurecomp% > tmp.closured.js
 
-@%jsexe% -cn tmp.closured.js compo_version.html
+@%jsexe% -cn -pn tmp.closured.js compo_version.html
 
 @REM TODO: Should %zip% here..
 @%zip% %ZIPNAME% compo_version.html debug_version.html %INFOFILE%
